9f02de99f5f96da9c7c8c5020290d28dda416c8f
luoleiself <luoleiself@163.com>
2022-05-16 10:14:03 +0800

    randomVersion

--- test/fileVersionRegExpTest.js
+++ test/fileVersionRegExpTest.js
@@ -72,8 +72,8 @@ body {
 
 // 日期时间格式化
 /**
- * @member dateFormat 日期时间戳格式化
- * @param {Number|String} timestamp 日期时间戳秒数
+ * @member dateFormat 日期时间格式化
+ * @param {Number|String|Object} paramDate 日期时间
  * @param {Object} param1 格式化参数配置
  * @param {Number} param1.type 格式化类型, 0 默认全部, 1 只日期部分, 2 只时间部分
  * @param {Boolean} param1.isShowSeparator 是否保留分隔符, 默认 true
@@ -81,79 +81,86 @@ body {
  * @param {String} param1.timeSeparator 时间部分分隔符, 默认 :
  * @returns
  */
-function dateFormat(timestamp, { type = 0, isShowSeparator = true, dateSeparator = '-', timeSeparator = ':' } = {}) {
-    const _date = new Date(timestamp * 1000);
-    const year = _date.getFullYear();
-    const month = _date.getMonth() + 1;
-    const date = _date.getDate();
+function dateFormat(paramDate, { type = 0, isShowSeparator = true, dateSeparator = '-', timeSeparator = ':' } = {}) {
+  let _date = null;
+  if (Object.prototype.toString.call(paramDate) == '[object Date]') {
+    _date = paramDate;
+  } else if (/^\d{1,}$/.test(paramDate)) {
+    _date = new Date(paramDate);
+  } else {
+    _date = new Date();
+  }
+  const year = _date.getFullYear();
+  const month = _date.getMonth() + 1;
+  const date = _date.getDate();
 
-    const hour = _date.getHours();
-    const min = _date.getMinutes();
-    const second = _date.getSeconds();
+  const hour = _date.getHours();
+  const min = _date.getMinutes();
+  const second = _date.getSeconds();
 
-    function gtNine(val) {
-        return val > 9 ? val : `0${val}`;
-    }
-    let result = `${year}${dateSeparator}${gtNine(month)}${dateSeparator}${gtNine(date)} ${gtNine(hour)}${timeSeparator}${gtNine(
+  function gtNine(val) {
+    return val > 9 ? val : `0${val}`;
+  }
+  let result = `${year}${dateSeparator}${gtNine(month)}${dateSeparator}${gtNine(date)} ${gtNine(hour)}${timeSeparator}${gtNine(
     min
   )}${timeSeparator}${gtNine(second)}`;
 
-    const dateRegExp = new RegExp(dateSeparator, 'gmi');
-    const timeRegExp = new RegExp(timeSeparator, 'gmi');
+  const dateRegExp = new RegExp(dateSeparator, 'gmi');
+  const timeRegExp = new RegExp(timeSeparator, 'gmi');
 
-    let [dateRes, timeRes] = result.split(' ');
+  let [dateRes, timeRes] = result.split(' ');
 
-    switch (type) {
-        case 1:
-            return isShowSeparator ? dateRes : dateRes.replace(dateRegExp, '');
-        case 2:
-            return isShowSeparator ? timeRes : timeRes.replace(timeRegExp, '');
-        default:
-            return isShowSeparator ? result : `${dateRes.replace(dateRegExp, '')}${timeRes.replace(timeRegExp, '')}`;
-    }
+  switch (type) {
+    case 1:
+      return isShowSeparator ? dateRes : dateRes.replace(dateRegExp, '');
+    case 2:
+      return isShowSeparator ? timeRes : timeRes.replace(timeRegExp, '');
+    default:
+      return isShowSeparator ? result : `${dateRes.replace(dateRegExp, '')}${timeRes.replace(timeRegExp, '')}`;
+  }
 }
 console.log(`dateFormat(1652441404) `, dateFormat(1652441404));
 console.log(
-    `dateFormat(1652441404, {type : 0, dateSeparator : '/', timeSeparator : '_', isShowSeparator :false}) `,
-    dateFormat(1652441404, { type: 0, dateSeparator: '/', timeSeparator: '_', isShowSeparator: false })
+  `dateFormat(1652441404, {type : 0, dateSeparator : '/', timeSeparator : '_', isShowSeparator :false}) `,
+  dateFormat(1652441404, { type: 0, dateSeparator: '/', timeSeparator: '_', isShowSeparator: false })
 );
 console.log(
-    `dateFormat(1652441404, {type : 1, dateSeparator : '/', timeSeparator : '_', isShowSeparator :false}) `,
-    dateFormat(1652441404, { type: 1, dateSeparator: '/', timeSeparator: '_', isShowSeparator: false })
+  `dateFormat(1652441404, {type : 1, dateSeparator : '/', timeSeparator : '_', isShowSeparator :false}) `,
+  dateFormat(1652441404, { type: 1, dateSeparator: '/', timeSeparator: '_', isShowSeparator: false })
 );
 console.log(
-    `dateFormat(1652441404, {type : 2, dateSeparator : '/', timeSeparator : '_', isShowSeparator :false}) `,
-    dateFormat(1652441404, { type: 2, dateSeparator: '/', timeSeparator: '_', isShowSeparator: false })
+  `dateFormat(1652441404, {type : 2, dateSeparator : '/', timeSeparator : '_', isShowSeparator :false}) `,
+  dateFormat(1652441404, { type: 2, dateSeparator: '/', timeSeparator: '_', isShowSeparator: false })
 );
 console.log(
-    `dateFormat(1652441404, {type : 3, dateSeparator : '/', timeSeparator : '_', isShowSeparator :false}) `,
-    dateFormat(1652441404, { type: 3, dateSeparator: '/', timeSeparator: '_', isShowSeparator: false })
+  `dateFormat(1652441404, {type : 3, dateSeparator : '/', timeSeparator : '_', isShowSeparator :false}) `,
+  dateFormat(1652441404, { type: 3, dateSeparator: '/', timeSeparator: '_', isShowSeparator: false })
 );
 console.log(
-    `dateFormat(1652441404, {type : 0, dateSeparator : '/', timeSeparator : '_', isShowSeparator :true}) `,
-    dateFormat(1652441404, { type: 0, dateSeparator: '/', timeSeparator: '_', isShowSeparator: true })
+  `dateFormat(1652441404, {type : 0, dateSeparator : '/', timeSeparator : '_', isShowSeparator :true}) `,
+  dateFormat(1652441404, { type: 0, dateSeparator: '/', timeSeparator: '_', isShowSeparator: true })
 );
 console.log(
-    `dateFormat(1652441404, {type : 1, dateSeparator : '/', timeSeparator : '_', isShowSeparator :true}) `,
-    dateFormat(1652441404, { type: 1, dateSeparator: '/', timeSeparator: '_', isShowSeparator: true })
+  `dateFormat(1652441404, {type : 1, dateSeparator : '/', timeSeparator : '_', isShowSeparator :true}) `,
+  dateFormat(1652441404, { type: 1, dateSeparator: '/', timeSeparator: '_', isShowSeparator: true })
 );
 console.log(
-    `dateFormat(1652441404, {type : 2, dateSeparator : '/', timeSeparator : '_', isShowSeparator :true}) `,
-    dateFormat(1652441404, { type: 2, dateSeparator: '/', timeSeparator: '_', isShowSeparator: true })
+  `dateFormat(1652441404, {type : 2, dateSeparator : '/', timeSeparator : '_', isShowSeparator :true}) `,
+  dateFormat(1652441404, { type: 2, dateSeparator: '/', timeSeparator: '_', isShowSeparator: true })
 );
 console.log(
-    `dateFormat(1652441404, {type : 3, dateSeparator : '/', timeSeparator : '_', isShowSeparator :true}) `,
-    dateFormat(1652441404, { type: 3, dateSeparator: '/', timeSeparator: '_', isShowSeparator: true })
+  `dateFormat(1652441404, {type : 3, dateSeparator : '/', timeSeparator : '_', isShowSeparator :true}) `,
+  dateFormat(1652441404, { type: 3, dateSeparator: '/', timeSeparator: '_', isShowSeparator: true })
 );
 
 // var result = str.match(/<(?:\blink\b|\bscript\b).*?(?:\bsrc\b|\bhref\b)=("|')(.*)\1.*?\/?>/gim);
 // var result = str.match(/(<(?:\blink\b|\bscript\b).*?(?:\bsrc\b|\bhref\b)=.*?)(?:(\.(?:css|js|map)\??))(.*?\/?>)/gim);
 // console.log(result);
-// 
-// 匹配 .css .js 文件 
+//
+// 匹配 .css .js 文件
 // (<(?:\blink\b|\bscript\b).*?(?:\bsrc\b|\bhref\b)=.*?)(?:(\.(?:css|js)\??))(.*?\/?>)
-// 
-// 
+//
+//
 // var date = Math.floor(Date.now() / 1000);
 // var r = str.replace(/(<(?:\blink\b|\bscript\b).*?(?:\bsrc\b|\bhref\b)=.*?)(?:(\.(?:css|js)\??))(.*?\/?>)/gim, function($f, $1, $2, $3) {
 //     if ($2.includes('?')) {
@@ -166,27 +173,36 @@ console.log(
 // console.log(r);
 //
 //
-// 匹配 ? 参数 
+// 匹配 ? 参数
 // (<(?:\blink\b|\bscript\b).*?(?:\bsrc\b|\bhref\b)=.*?\.(?:css|js))(\?[^<>\/]*)?(.*?\/?>)
-// 
-var date = Math.floor(Date.now() / 1000);
+//
+var date = Date.now();
 // (<(?:\blink\b|\bscript\b|img).*?(?:\bsrc\b|\bhref\b)=.*?\.(?:css|js|png|jpg|jpeg|bmp|gif|webp))(\?[^<>"']*)?(.*?\/?>)
 // '(<(?:\\blink\\b|\\bscript\\b|img).*?(?:\bsrc\b|\bhref\b)=.*?\\.(?:'+ 'css|js|png|jpg|jpeg|bmp|gif|webp' +'))(\\?[^<>\"\']*)?(.*?\\/?>)'
-var reg = new RegExp("(<(?:"+ "\\blink\\b|\\bscript\\b|img" +").*?(?:"+ "\\bsrc\\b|\\bhref\\b" +")=.*?\\.(?:"+ "css|js|png|jpg|jpeg|bmp|gif|webp" + "))(\\?[^<>\\\"\\']*)?(.*?\\/?>)", 'gmi');
+var reg = new RegExp(
+  '(<(?:' +
+    '\\blink\\b|\\bscript\\b|img' +
+    ').*?(?:' +
+    '\\bsrc\\b|\\bhref\\b' +
+    ')=.*?\\.(?:' +
+    'css|js|png|jpg|jpeg|bmp|gif|webp' +
+    '))(\\?[^<>\\"\\\']*)?(.*?\\/?>)',
+  'gmi'
+);
 // var reg = new RegExp('(<(?:\\blink\\b|\\bscript\\b).*?(?:\\bsrc\\b|\\bhref\\b)=.*?\\.(?:' + 'css|js' + '))(\\?[^<>\\/]*)?(.*?\\/?>)', 'gmi');
-var r = str.replace(reg, function($f, $1, $2, $3) {
-    // 没有匹配到 ? 参数
-    if (!$2) {
-        return `${$1}?v=${dateFormat(date, { isShowSeparator: false })}${$3}`;
-    }
-    // ? 参数中包含 v 参数
-    var result = $2.match(/((v=)([^&]*))/gmi);
-    if (Array.isArray(result)) {
-        $2 = $2.replace(/((v=)([^&]*))/gmi, `$2${dateFormat(date, { isShowSeparator: false })}`); // 替换 v 参数
-        return `${$1}${$2}${$3}`;
-    }
-    // 有 ? 但没有 v 参数, 可能有其他参数
-    var arr = $2.split('?');
-    return `${$1}${arr[0]}?v=${dateFormat(date, { isShowSeparator: false })}${arr[1] != '' ? `&${arr[1]}`: ''}${$3}`;
+var r = str.replace(reg, function ($f, $1, $2, $3) {
+  // 没有匹配到 ? 参数
+  if (!$2) {
+    return `${$1}?v=${dateFormat(date, { isShowSeparator: false })}${$3}`;
+  }
+  // ? 参数中包含 v 参数
+  var result = $2.match(/((v=)([^&]*))/gim);
+  if (Array.isArray(result)) {
+    $2 = $2.replace(/((v=)([^&]*))/gim, `$2${dateFormat(date, { isShowSeparator: false })}`); // 替换 v 参数
+    return `${$1}${$2}${$3}`;
+  }
+  // 有 ? 但没有 v 参数, 可能有其他参数
+  var arr = $2.split('?');
+  return `${$1}${arr[0]}?v=${dateFormat(date, { isShowSeparator: false })}${arr[1] != '' ? `&${arr[1]}` : ''}${$3}`;
 });
 console.log(r);
